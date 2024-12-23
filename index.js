810b56c2b1a79b89441e785548cdd83c27866bbb
Flint Rose <oliverflintrose@gmail.com>
2024-12-22 20:26:23 -0600

    adding readings and discord msgs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Client, Events, GatewayIntentBits, Intents } = require("discord.js");
 const { botKey } = require("./config.json");
 const { exec } = require("node:child_process");
 const { spawn } = require("node:child_process");
+const si = require("systeminformation");
 
 const client = new Client({
   intents: [
@@ -21,8 +22,34 @@ client.login(botKey);
 
 client.once("ready", async () => {
   console.log("Arceus is alive, starting server.");
+  console.log(statusCheck());
   serverStart();
 });
+
+/*
+si.cpuTemperature(cb)
+si.cpuCurrentSpeed(cb)
+si.mem(cb)
+
+*/
+
+function statusCheck() {
+  si.cpuTemperature((data) => {
+    console.log("temp:");
+    console.log(data);
+    if (data.main > 60) {
+      console.log("CPU is overheating"); //change this to a discord msg
+    }
+  });
+  si.cpuCurrentSpeed((data) => {
+    console.log("speed:");
+    console.log(data);
+  });
+  si.mem((data) => {
+    console.log("data");
+    console.log(data);
+  });
+}
 
 function serverStart() {
   const channel = client.channels.cache.get("1112805993286484059"); //get channel by ID
@@ -30,10 +57,12 @@ function serverStart() {
   //channel.send("Server is resetting in 30 seconds...I recommend you land");
   const command = spawn("sh", ["run.sh"]);
   console.log("process started");
-  channel.send("Server is online.");
+  //channel.send("Server is online.");
   command.on("exit", (code) => {
     channel.send("Server has shutdown, restarting now...");
-    serverStart();
+    setTimeout(() => {
+      serverStart();
+    }, 5000);
   });
   command.stdout.on("data", (data) => {
     console.log(data.toString());
@@ -41,6 +70,7 @@ function serverStart() {
   command.stderr.on("data", (data) => {
     console.error(data.toString());
   }); //prints errors
+
   setTimeout(() => {
     command.stdin.write(
       "/tell @a Server is resetting in 30 seconds...I recommend you land\n"
